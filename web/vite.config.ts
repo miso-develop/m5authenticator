e4929587abf7fa306647ff1978a25018eb46bb00
Miso Tanaka <22117028+miso-develop@users.noreply.github.com>
2026-09-10 13:07:26 +0900

    build: include provisioner and flasher pages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/m5authenticator/",
+  build: {
+    rollupOptions: {
+      input: {
+        provisioner: "index.html",
+        flasher: "flash.html",
+      },
+    },
+  },
   server: {
     host: "127.0.0.1",
     hmr: false,
