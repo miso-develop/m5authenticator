5bfe5ea903f2e32b6bef1289016ec0d25cbaf165
Miso Tanaka <22117028+miso-develop@users.noreply.github.com>
2026-09-14 12:53:52 +0900

    build: add fnm cmd auto-switch hook

---
+++ scripts/fnm_autorun.cmd
@@ -0,0 +1,5 @@
+@echo off
+if not defined FNM_AUTORUN_GUARD (
+  set "FNM_AUTORUN_GUARD=AutorunGuard"
+  FOR /f "tokens=*" %%z IN ('fnm env --use-on-cd --version-file-strategy recursive') DO CALL %%z
+)
