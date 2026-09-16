444756f4495d70abcfcbb6ab78c24b7d8dd5a73f
Miso Tanaka <22117028+miso-develop@users.noreply.github.com>
2026-09-17 00:46:18 +0900

    refactor(provisioning): preserve existing VMK sink interface

--- firmware/components/m5auth_provisioning/include/m5auth/provisioning/session_protocol_v2.hpp
+++ firmware/components/m5auth_provisioning/include/m5auth/provisioning/session_protocol_v2.hpp
@@ -38,8 +38,7 @@ public:
     virtual ~SessionV2VmkSink() = default;
     virtual bool install_vmk(
         const session::protocol_v2::BeginContext& context,
-        const session::Vmk& vmk,
-        std::uint64_t now_ms
+        const session::Vmk& vmk
     ) = 0;
 };
 
