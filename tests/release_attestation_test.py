bb9a58b48c2209b3dfd29fc32703d1330f6d473e
Miso Tanaka <22117028+miso-develop@users.noreply.github.com>
2026-09-20 12:44:11 +0900

    test: fix recovery source fixture literal

--- tests/release_attestation_test.py
+++ tests/release_attestation_test.py
@@ -15,7 +15,7 @@ import esp_idf_build_image
 import release_attestation
 
 
-SOURCE_SHA = "996378b07d8587c0d11e43362590e5d1062ad8c2
+SOURCE_SHA = "996378b07d8587c0d11e43362590e5d1062ad8c2"
 WORKFLOW_SHA = "b" * 40
 
 
